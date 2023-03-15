0c057618de37f55393b5eeb72f9d451901beaeb5
Igor Entaev <entaev@mail.ru>
2023-03-15 13:28:59 +0800

    вывод юзеров в таблицу

--- src/main/resources/templates/js/main.js
+++ src/main/resources/templates/js/main.js
@@ -1,21 +1,38 @@
 const url = 'http://localhost:8081/api/admin'
 const container = document.querySelector('tbody')
 let result = ''
-const newForm = document.getElementById("new-form")
-let newEmail = document.getElementById("new_email")
-let new_password = document.getElementById("new_password")
-let new_age = document.getElementById("new_age")
+const newForm = document.getElementById('new-form')
+let newEmail = document.getElementById('new_email')
+let new_password = document.getElementById('new_password')
+let new_age = document.getElementById('new_age')
+let opcion = ''
 
+const myModalNewUser = new bootstrap.Modal(document.getElementById('new_user'))
+const link = document.getElementById('profile-tab');
+link.addEventListener('click', () => {
+    myModalNewUser.show()
+})
 
-const showData = (arrays) => {
-    arrays.forEach(arr => {
+const showData = (users) => {
+    console.log(users)
+    users.forEach(user => {
         result += `
     <tr>
-    <td>${arr.id}</td>
-    <td>${arr.newEmail}</td>
+    <td>${user.id}</td>
+    <td>${user.email}</td>
+    <td>${user.age}</td>
+    <td>${user.roles}</td>
 </tr>
     `
     })
+//     for (let i = 0; i < data.length; i++) {
+//         result += `<tr><td>${data[i].id}</td>
+//                     <td>${data[i].email}</td>
+//                     <td>${data[i].age}</td>
+//                     <td>${data[i].roles}</td>
+//                     </tr>`
+//     }
+
     container.innerHTML = result
 }
 
