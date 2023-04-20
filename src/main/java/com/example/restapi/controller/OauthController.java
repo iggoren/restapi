59be101a9adc86f4b6515f62ae1986eb5cf7d658
Igor Entaev <entaev@mail.ru>
2023-04-20 15:43:54 +0800

    отправка на gmail и редирект

--- src/main/java/com/example/restapi/controller/OauthController.java
+++ src/main/java/com/example/restapi/controller/OauthController.java
@@ -4,23 +4,23 @@ package com.example.restapi.controller;
 import com.github.scribejava.apis.GoogleApi20;
 import com.github.scribejava.core.builder.ServiceBuilder;
 import com.github.scribejava.core.oauth.OAuth20Service;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class OauthController {
 
     private String clientId = "823444065671-s81c2u14s4bc5src79g91aqtmhgjh5ug.apps.googleusercontent.com";
     private String clientSecret = "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";
-    private String callbackUrl = "http://localhost:8082/login/oauth2/code/google";
+    private String callbackUrl = "http://localhost:8082/admin";
 
 
 
 //        private static HttpTransport httpTransport;
 //        private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
 
-        @GetMapping("/google/auth-url")
-        @ResponseBody
-        public String getGoogleAuthUrl() {
+        @GetMapping("oauth2/google/auth-url")
+                public String getGoogleAuthUrl() {
 
 
 
@@ -28,10 +28,18 @@ public class OauthController {
             return "{\"authUrl\": \"" + authorizationUrl + "\"}";
         }
 
+    @GetMapping("admin/oauth")
+    public String getAuth() {
 
+
+
+
+        return  "redirect:/admin";
+    }
     OAuth20Service service = new ServiceBuilder(clientId)
             .apiSecret(clientSecret)
             .callback(callbackUrl)
+            .defaultScope("https://www.googleapis.com/auth/calendar")
             .build(GoogleApi20.instance());
 
 
